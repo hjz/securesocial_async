8e3cf979b70f1e5055f32b5497448105553b3255
jang1lee <jang1lee@manaolabs.com>
2012-12-04 13:28:27 +0900

    fixed LinkedIn error

--- module-code/app/securesocial/core/providers/LinkedInProvider.scala
+++ module-code/app/securesocial/core/providers/LinkedInProvider.scala
@@ -89,6 +89,6 @@ object LinkedInProvider {
   val Id = "id"
   val FirstName = "firstName"
   val LastName = "lastName"
-  val FormattedName = "formatted-name"
+  val FormattedName = "formattedName"
   val PictureUrl = "pictureUrl"
 }
